56219076004f52524fb6b3814357bc4fca15351d
RxVince <vrx@winstory.io>
2025-10-24 17:16:38 +0200

    new icons creation and completion /welcome + /mywin

--- components/icons/CompletionIcon.tsx
+++ components/icons/CompletionIcon.tsx
@@ -1,8 +1,5 @@
 const CompletionIcon = () => (
-    <svg width="122" height="58" viewBox="0 0 122 58" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M62.6667 45.9167V12.0833L103.458 29L62.6667 45.9167ZM70.0833 37.0958L89.5521 29L70.0833 20.9042V37.0958Z" fill="#FFE100"/>
-      <path d="M29.6667 45.9167V12.0833L70.4583 29L29.6667 45.9167ZM37.0833 37.0958L56.5521 29L37.0833 20.9042V37.0958Z" fill="#FFE100"/>
-    </svg>
-  );
+  <img src="/completion.svg" alt="Completion Icon" width="244" height="116" />
+);
   export default CompletionIcon;
   
